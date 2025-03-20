1fdc8ed3b162e6a806b2933b94cac1ca917e2fec
diespensary <diespensary@icloud.com>
2025-03-20 15:25:19 +0400

    models

---
+++ src/main/java/com/example/nexigntesttask/models/CdrRecord.java
@@ -0,0 +1,35 @@
+package com.example.nexigntesttask.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cdr_records")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CdrRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @Column(name = "initiator_msisdn", nullable = false)
+    private String initiatorMsisdn;
+
+    @Column(name = "recipient_msisdn", nullable = false)
+    private String recipientMsisdn;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+}
---
+++ src/main/java/com/example/nexigntesttask/models/Subscriber.java
@@ -0,0 +1,21 @@
+package com.example.nexigntesttask.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "subscribers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subscriber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "msisdn", nullable = false, unique = true)
+    private String msisdn;
+}
