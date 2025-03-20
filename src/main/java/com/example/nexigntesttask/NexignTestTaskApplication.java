c260512d11fd9c9203b801a3e7ee08dbd7ef2c54
diespensary <diespensary@icloud.com>
2025-03-20 15:25:10 +0400

    pom and application

---
+++ src/main/java/com/example/nexigntesttask/NexignTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.nexigntesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexignTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexignTestTaskApplication.class, args);
+    }
+
+}
